e40926f3ca6ccd32a3284062d7b4bcea1ea9d66a
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-07 17:04:23 +0100

    fix to cycle for with module operators

--- script.js
+++ script.js
@@ -25,19 +25,17 @@ buzz = 'buzz';
 fizzBuzz = 'fizz-buzz'
 
 for (let i = 1; i <= 100; i++){
-    console.log(i);
-
-    if (i % 3 === 0) {
-        console.log (fizz);
-    } 
-
-    if (i % 5 === 0) {
-        console.log (buzz);
-    }
 
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log (fizzBuzz);
+      console.log (fizzBuzz);
+    } else if (i % 3 === 0) {
+      console.log (fizz);
+    } else if (i % 5 === 0) {
+      console.log (buzz);
+    } else {
+      console.log(i);
     }
+
 }
 
 
