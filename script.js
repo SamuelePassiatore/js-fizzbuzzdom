958db25814498698628ca8b5f3700cc611683eeb
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-07 20:08:04 +0100

    print variables on page

--- script.js
+++ script.js
@@ -9,34 +9,37 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto 
 /*
 1 - Prendo gli elementi in pagina.
 2 - Creo un ciclo in console con i numeri da 1 a 100.
-3 - Creo un'istruzione condizionale con i multipli di tre e stampo "Fizz" al posto del numero.
-4 - Creo un'istruzione condizionale con i multipli di cinque e stampo "Buzz" al posto del numero.
-5 - Creo un'istruzione condizionale con i multipli sia di tre che di cinque e stampo "FizzBuzz" al posto del numero.
+3 - Creo un'istruzione condizionale.
+4 - Stampo le variabili in pagina.
+
+
 */
 
 // 1 - Prendo gli elementi in pagina.
-let fizz = document.getElementById('fizz');
-let buzz = document.getElementById('buzz');
-let fizzBuzz = document.getElementById('fizz-buzz');
+const table = document.getElementById('table');
 
-// 2 - Creo un ciclo in console con i numeri da 1 a 100.
-fizz = 'fizz';
-buzz = 'buzz';
-fizzBuzz = 'fizz-buzz'
+// 2 - 3 - 4 Creo un ciclo in console con i numeri da 1 a 100.
+let fizzBuzz = 'fizzBuzz';
+let fizz = 'fizz';
+let buzz = 'buzz';
 
 for (let i = 1; i <= 100; i++){
-
     if (i % 3 === 0 && i % 5 === 0) {
       console.log (fizzBuzz);
+      table.innerText += fizzBuzz;
     } else if (i % 3 === 0) {
       console.log (fizz);
+      table.innerText += fizz;
     } else if (i % 5 === 0) {
       console.log (buzz);
+      table.innerText += buzz;
     } else {
       console.log(i);
+      table.innerText += i;
     }
-
 }
+
+
 
 
 
