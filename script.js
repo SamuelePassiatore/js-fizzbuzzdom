3cdd7c8ae1fecd86b044d5459c4dcb383b4f5618
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-07 15:03:29 +0100

    print number from 0 to 100 in console

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ ma che per i multipli di 3 stampi “Fizz” al posto del numero,
 per i multipli di 5 stampi “Buzz” al posto del numero.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
 */
+
+/*
+1 - Prendo gli elementi in pagina.
+2 - Creo un ciclo in console con i numeri da 1 a 100.
+3 - Creo un'istruzione condizionale con i multipli di tre e stampo "Fizz" al posto del numero.
+4 - Creo un'istruzione condizionale con i multipli di cinque e stampo "Buzz" al posto del numero.
+5 - Creo un'istruzione condizionale con i multipli sia di tre che di cinque e stampo "FizzBuzz" al posto del numero.
+*/
+
+// 1 - Prendo gli elementi in pagina.
+
+// 2 - Creo un ciclo in console con i numeri da 1 a 100.
+for (let i = 1; i <= 100; i++){
+    console.log(i);
+}
