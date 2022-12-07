a3dd372067ead0e76d5b9a77d021ddc39f923be1
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-07 22:32:03 +0100

    added elements to DOM & added style to them

--- script.js
+++ script.js
@@ -11,33 +11,43 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto 
 2 - Creo un ciclo in console con i numeri da 1 a 100.
 3 - Creo un'istruzione condizionale.
 4 - Stampo le variabili in pagina.
-
-
 */
 
 // 1 - Prendo gli elementi in pagina.
-const table = document.getElementById('table');
+let element = document.getElementById('element');
 
-// 2 - 3 - 4 Creo un ciclo in console con i numeri da 1 a 100.
+// 2 - 3 Creo un ciclo in console con i numeri da 1 a 100.
 let fizzBuzz = 'fizzBuzz';
 let fizz = 'fizz';
 let buzz = 'buzz';
 
+// Variabili d'appoggio
+let content = '';
+
 for (let i = 1; i <= 100; i++){
     if (i % 3 === 0 && i % 5 === 0) {
       console.log (fizzBuzz);
-      table.innerText += fizzBuzz;
+      content += `<div class="bg-custom-red fw-bold">${fizzBuzz}</div>`;
     } else if (i % 3 === 0) {
       console.log (fizz);
-      table.innerText += fizz;
+      content += `<div class="bg-custom-green fw-bold">${fizz}</div>`;
     } else if (i % 5 === 0) {
       console.log (buzz);
-      table.innerText += buzz;
+      content += `<div class="bg-custom-yellow fw-bold">${buzz}</div>`;
     } else {
       console.log(i);
-      table.innerText += i;
+      content += `<div class="bg-custom-light-blue fw-bold">${i}</div>`;
     }
 }
+
+// 4 - Stampo la variabile d'appoggio in pagina
+element.innerHTML = content;
+
+
+
+
+
+
 
 
 
